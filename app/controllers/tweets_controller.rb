80e9c7b9e4b5747fc2811bc6cd2367e532adb1ec
Blessing Ubogu <60205509+BlessingUb@users.noreply.github.com>
2021-10-24 02:10:30 +0100

    users can create tweets and see all tweets  in the /tweets route

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,29 @@
 class TweetsController < ApplicationController
+    def index
+      @tweets = Tweet.all
+    end
+    def show 
+      @tweet = Tweet.find(params[:id])
+    end
+  
+    def new
+      @tweet = Tweet.new
+    end
+  
+    def create
+      @tweet = Tweet.new(tweet_params)
+  
+      if @tweet.save
+        redirect_to tweets_path
+      else
+        render :new
+      end
+    end
+  
+    private
+    def tweet_params
+      params.require(:tweet).permit(:message)
+    end
+    
+  
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,3 @@
 class Tweet < ApplicationRecord
+  validates :message, presence: true, length: { minimum: 3 }
 end
