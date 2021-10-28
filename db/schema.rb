d83a7a7bbf40c5948dc5e80a25e6b5602d5ce028
Blessing Ubogu <60205509+BlessingUb@users.noreply.github.com>
2021-10-28 11:48:40 +0100

    schema created forassociation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_222345) do
+ActiveRecord::Schema.define(version: 2021_10_28_104144) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,13 +21,17 @@ ActiveRecord::Schema.define(version: 2021_10_24_222345) do
     t.bigint "tweet_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["tweet_id"], name: "index_comments_on_tweet_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "tweets", force: :cascade do |t|
     t.string "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tweets_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -44,4 +48,6 @@ ActiveRecord::Schema.define(version: 2021_10_24_222345) do
   end
 
   add_foreign_key "comments", "tweets"
+  add_foreign_key "comments", "users"
+  add_foreign_key "tweets", "users"
 end
