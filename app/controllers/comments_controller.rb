01f722824c3a83551fa9c2e1efe8ec2d9ac23270
Blessing Ubogu <60205509+BlessingUb@users.noreply.github.com>
2021-10-28 14:16:49 +0100

    comment table created and controller generated

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+  def create
+    tweet = Tweet.find( params[:tweet_id] )
+    @comment = @tweet.comments.create(comment_params)
+    redirect_to root_path
+  end
+ private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
+end
