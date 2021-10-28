8d572390c2cb29af6597902905091c0aac8af158
Blessing Ubogu <60205509+BlessingUb@users.noreply.github.com>
2021-10-28 13:57:33 +0100

    deleted the comment table for a new table

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_28_104144) do
+ActiveRecord::Schema.define(version: 2021_10_28_125529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.bigint "tweet_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
-    t.index ["tweet_id"], name: "index_comments_on_tweet_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
-  end
 
   create_table "tweets", force: :cascade do |t|
     t.string "message"
@@ -47,7 +36,5 @@ ActiveRecord::Schema.define(version: 2021_10_28_104144) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "comments", "tweets"
-  add_foreign_key "comments", "users"
   add_foreign_key "tweets", "users"
 end
