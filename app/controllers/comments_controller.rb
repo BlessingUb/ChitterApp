ed0aa53f24ae7c822aceaffaa4d926531e5b9dc9
Blessing Ubogu <blessingubogu@Blessings-MacBook-Air.local>
2021-11-23 18:23:21 +0000

    redirect the comment feature to the main page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,13 +2,15 @@ class CommentsController < ApplicationController
 
   def create
     @tweet = Tweet.find( params[:tweet_id] )
-    @comment = @tweet.comments.create(comment_params)
+    @comment = @tweet.comments.new(comment_params)
     respond_to do |format|
-      format.html {redirect_to @tweet }
+      if @comment.save
+      format.html {redirect_to tweets_path, notice: 'Comment was successfully created.' }
       format.js 
+      end
     end
     
- end
+  end
  private
     def comment_params
       params.require(:comment).permit( :message, :user_id, :tweet_id )
