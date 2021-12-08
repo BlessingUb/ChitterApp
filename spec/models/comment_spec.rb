95c6d1a45492a7923420f62d410766066cbff8d9
Blessing Ubogu <blessingubogu@Blessings-MacBook-Air.local>
2021-12-08 13:56:40 +0000

    test written and registration page edited

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+    current_user = User.first_or_create!(firstname: 'bee', lastname: 'dee', username: 'beedee', bio: 'i am a girl', location: 'dundee', email: 'bee@example.com', password: 'beedee2', password_confirmation: 'beedee2')
+    tweet = Tweet.new(
+        message: 'Hey girl',
+        user: current_user
+      )
+    it 'should validate presence of a message' do
+      comment = tweet.comments.new(
+        message: '',
+        user_id: 1,
+        tweet_id: 1,
+      )
+      expect(comment).to_not be_valid
+      comment.message = 'hey how are you'
+      expect(tweet).to be_valid
+  
+    end
+  
+    
+  end
+  
--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -2,9 +2,26 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe Tweet, type: :model do
-  it 'should validate presence of body' do
-    tweet = Tweet.new
-    tweet.valid?
-    expect(tweet.errors.messages[:body]).to include "can't be blank"
+  current_user = User.first_or_create!(firstname: 'bee', lastname: 'dee', username: 'beedee', bio: 'i am a girl', location: 'dundee', email: 'bee@example.com', password: 'beedee2', password_confirmation: 'beedee2')
+  it 'should validate presence of a message' do
+    tweet = Tweet.new(
+      message: '',
+      user: current_user
+    )
+    expect(tweet).to_not be_valid
+    tweet.message = 'hey girl'
+    expect(tweet).to be_valid
+
+  end
+
+  it 'message should be 3 or more character long' do
+    tweet = Tweet.new(
+      message: 'Hi',
+      user: current_user
+    )
+    expect(tweet).to_not be_valid
+    tweet.message = 'hey girl'
+    expect(tweet).to be_valid
+
   end
 end
